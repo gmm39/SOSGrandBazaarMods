a3108806e8182d1357ebce71f5ad45691e005f2c
gmm39 <77864040+gmm39@users.noreply.github.com>
2025-09-16 10:51:44 -0500

    Add files via upload

---
+++ AnimationSpeedTweaks/AnimationSpeedTweaks/obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AnimationSpeedTweaks
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "AnimationSpeedTweaks";
+        public const string PLUGIN_NAME = "AnimationSpeedTweaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
