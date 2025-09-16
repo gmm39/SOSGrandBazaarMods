24112191173801446a430704ad936366a061552c
Gage <gmckenzie0101@gmail.com>
2025-09-16 11:08:05 -0500

    References fix

--- TestApp/TestApp/obj/Debug/net6.0/TestApp.AssemblyInfo.cs
+++ TestApp/TestApp/obj/Debug/net6.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce91b7f461d6f3777004f592e54359cf26039e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
