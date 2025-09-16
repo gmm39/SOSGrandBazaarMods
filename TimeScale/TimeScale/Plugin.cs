8229da8eff3f873eb4e36fe49b7e73cb4b52df3a
gmm39 <77864040+gmm39@users.noreply.github.com>
2025-09-16 10:57:10 -0500

    Add files via upload

---
+++ TimeScale/TimeScale/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using HarmonyLib;
+
+namespace TimeScale;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    private static ConfigEntry<float> TimeScaleEntry;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        TimeScaleEntry = Config.Bind("General",
+            "TimeScale",
+            30f,
+            "Scalar for game time. GameDefault: 60 | HalfSpeed: 30");
+
+        var Log = this.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Log.LogInfo($"TimeScale: {TimeScaleEntry.Value}");
+
+        Harmony.CreateAndPatchAll(typeof(DateManagerPatch));
+    }
+
+    private static class DateManagerPatch
+    {
+        [HarmonyPatch(typeof(DateManager), "Update")]
+        [HarmonyPrefix]
+        private static void Update_Prefix(DateManager __instance)
+        {
+            __instance.TimeScale = TimeScaleEntry.Value;
+        }
+    }
+}
---
+++ TimeScale/TimeScale/obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace TimeScale
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "TimeScale";
+        public const string PLUGIN_NAME = "TimeScale";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
