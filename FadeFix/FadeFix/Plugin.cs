5f724b9b05c1a04c14567d5e28982d343b3f6f2f
gmm39 <77864040+gmm39@users.noreply.github.com>
2025-09-16 10:53:57 -0500

    Add files via upload

---
+++ FadeFix/FadeFix/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using HarmonyLib;
+using Vector3 = UnityEngine.Vector3;
+
+namespace FadeFix;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        Harmony.CreateAndPatchAll(typeof(Plugin.FadeManagerPatch), null);
+    }
+    
+    private static class FadeManagerPatch
+    {
+        [HarmonyPatch(typeof(FadeManager), "Awake")]
+        [HarmonyPrefix]
+        private static void Update_Prefix(FadeManager __instance)
+        {
+            __instance.fadeParent.localScale = new Vector3(1.5f, 1.5f, 1.0f);
+        }
+    }
+}
---
+++ FadeFix/FadeFix/obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FadeFix
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "FadeFix";
+        public const string PLUGIN_NAME = "FadeFix";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
