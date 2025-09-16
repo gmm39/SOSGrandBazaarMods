599e04f9cacecc9aec9619363c65ddbdbd938343
gmm39 <77864040+gmm39@users.noreply.github.com>
2025-09-16 10:56:33 -0500

    Add files via upload

---
+++ StackSizeTweak/StackSizeTweak/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BokuMono;
+using HarmonyLib;
+
+namespace StackSizeTweak;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    private new static ManualLogSource Log;
+    private static ConfigEntry<int> StackSize;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        StackSize = Config.Bind("ItemSettings", "StackSize", 99,
+            "Enter max stack size. Over 9999 is untested!");
+        
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(typeof(StackPatch1));
+    }
+
+    private static class StackPatch1
+    {
+        [HarmonyPatch(typeof(UITitleMainPage), "PlayTitleLogoAnimation")]
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            foreach (var item in ManagedSingleton<MasterDataManager>.Instance.ItemMasterData) 
+                if (item.StackSize != 1) item.StackSize = StackSize.Value;
+        }
+    }
+}
---
+++ StackSizeTweak/StackSizeTweak/obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace StackSizeTweak
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "StackSizeTweak";
+        public const string PLUGIN_NAME = "StackSizeTweak";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
