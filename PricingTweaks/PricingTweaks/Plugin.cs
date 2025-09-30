57f4046add4eb3ea0a4af8965bbd8957ee4f08f3
Gage <gmckenzie0101@gmail.com>
2025-09-30 12:10:28 -0500

    Added Clothing and Item tweaks

--- PricingTweaks/PricingTweaks/Plugin.cs
+++ PricingTweaks/PricingTweaks/Plugin.cs
@@ -18,10 +18,17 @@ public class Plugin : BasePlugin
     private static ConfigEntry<bool> Enable_01_ANIMALS;
     private static ConfigEntry<bool> Enable_02_EXPANSIONS;
     private static ConfigEntry<bool> Enable_03_DECOR;
+    private static ConfigEntry<bool> Enable_04_CLOTHING;
+    private static ConfigEntry<bool> Enable_05_ITEMS;
 
     private static ConfigEntry<float> AnimalBazaarPriceMulti;
     private static ConfigEntry<float> ExpansionPriceMulti;
     private static ConfigEntry<float> DecorPriceMulti;
+    private static ConfigEntry<float> ClothingPriceMulti;
+    private static ConfigEntry<float> ShopBuyPriceMulti;
+    private static ConfigEntry<float> ShopSalePriceMulti;
+    private static ConfigEntry<float> BazaarSalePriceMulti;
+    private static ConfigEntry<float> TrendPriceMulti;
 
     public override void Load()
     {
@@ -30,15 +37,30 @@ public class Plugin : BasePlugin
             "Enable tweaks in 01 ANIMALS");
         Enable_02_EXPANSIONS = Config.Bind("-----00 FEATURE SELECT-----", "Enable_02_EXPANSIONS", true,
             "Enable tweaks in 02 EXPANSIONS");
-        Enable_03_DECOR = Config.Bind("-----00 FEATURE SELECT-----", "Enable_02_DECOR", true,
+        Enable_03_DECOR = Config.Bind("-----00 FEATURE SELECT-----", "Enable_03_DECOR", true,
             "Enable tweaks in 03 DECOR");
+        Enable_04_CLOTHING = Config.Bind("-----00 FEATURE SELECT-----", "Enable_04_CLOTHING", true,
+            "Enable tweaks in 04 CLOTHING");
+        Enable_05_ITEMS = Config.Bind("-----00 FEATURE SELECT-----", "Enable_05_ITEMS", true,
+            "Enable tweaks in 05 ITEMS");
 
         AnimalBazaarPriceMulti = Config.Bind("-----01 ANIMALS-----", "Animal_Bazaar_Price_Multiplier", 1.0f,
-            "Multiplies the bazaar animal prices by the given amount.");
+            "Multiplies bazaar animal prices by the given amount.");
         ExpansionPriceMulti = Config.Bind("-----02 EXPANSIONS-----", "Expansion_Price_Multiplier", 1.0f,
             "Multiplies the expansion prices by the given amount.");
         DecorPriceMulti = Config.Bind("-----03 DECOR-----", "Decor_Price_Multiplier", 1.0f,
-            "Multiplies the decor prices by the given amount.");
+            "Multiplies decor prices by the given amount.");
+        ClothingPriceMulti = Config.Bind("-----04 CLOTHING-----", "Clothing_Price_Multiplier", 1.0f,
+            "Multiplies clothing prices by the given amount.");
+        
+        ShopBuyPriceMulti = Config.Bind("-----05 ITEMS-----", "Shop_Buy_Price_Multiplier", 1.4f,
+            "Multiplies item prices when buying from a shop by the given amount. GameDefault: 1.4");
+        ShopSalePriceMulti = Config.Bind("-----05 ITEMS-----", "Shop_Sale_Price_Multiplier", 1.0f,
+            "Multiplies item prices when selling to a shop by the given amount.");
+        BazaarSalePriceMulti = Config.Bind("-----05 ITEMS-----", "Bazaar_Sale_Price_Multiplier", 1.0f,
+            "Multiplies item prices when selling at the bazaar by the given amount.");
+        TrendPriceMulti = Config.Bind("-----05 ITEMS-----", "Trend_Price_Multiplier", 1.3f,
+            "Multiplies item prices when buying from a shop by the given amount. GameDefault: 1.3");
 
         Log = base.Log;
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
@@ -55,6 +77,16 @@ public class Plugin : BasePlugin
             if (Enable_01_ANIMALS.Value) AnimalPriceTweak();
             if (Enable_02_EXPANSIONS.Value) ExpansionPriceTweak();
             if (Enable_03_DECOR.Value) DecorStallPriceTweak();
+            if (Enable_04_CLOTHING.Value) ClothingPriceTweak();
+            if (Enable_05_ITEMS.Value)
+            {
+                var gSetting = SettingAssetManager.Instance.GameSetting;
+                
+                gSetting.BuyPriceMagnification = ShopBuyPriceMulti.Value;
+                gSetting.SaleShopPriceMagnification = ShopSalePriceMulti.Value;
+                gSetting.SaleBazaarPriceMagnification = BazaarSalePriceMulti.Value;
+                gSetting.SaleTrendPriceMagnification = TrendPriceMulti.Value;
+            }
         }
 
         private static void AnimalPriceTweak()
@@ -93,6 +125,27 @@ public class Plugin : BasePlugin
             foreach (var item in MasterDataManager.Instance.ItemMasterData)
                 if (decorShopIds.Contains(item.Id))
                     item.Price = (int)(item.Price * DecorPriceMulti.Value);
+        }
+        
+        private static void ClothingPriceTweak()
+        {
+            foreach (var item in MasterDataManager.Instance.ShopAvatarMakeMaster.list)
+            {
+                try
+                {
+                    var priceProp = typeof(ShopAvatarMakeMasterData).GetProperty("Price");
+                    if (priceProp != null && priceProp.CanWrite)
+                    {
+                        priceProp.SetValue(item,
+                            (int)(System.Convert.ToInt32(priceProp.GetValue(item)) * ClothingPriceMulti.Value));
+                    }
+                }
+                catch (System.NullReferenceException e)
+                {
+                    Log.LogInfo(e.Message);
+                    return;
+                }
+            }
         }
     }
 }
